b4b791c5fcee5611dfc138753d1f31f4359d9bd7
Murtaza <murtazamandasore@gmail.com>
2026-06-13 23:35:48 +0500

    Change from Homescreen to LoginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simpleapp/View/Screens/HomeScreen.dart';
+import 'package:simpleapp/View/Screens/LoginScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Homescreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Loginscreen());
   }
 }
